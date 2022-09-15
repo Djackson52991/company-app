720dafde1b772793b19ecbc38c68d290661c3405
djackson52991 <donovanjackson1991@gmail.com>
2022-09-14 23:53:29 -0400

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import shellImage from './img/shell.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shell Company 123456789</h1>
+      <h2>Auther: Donovan Jackson</h2>
+      <img src={shellImage} alt="profile-image" />
     </div>
   );
 }
